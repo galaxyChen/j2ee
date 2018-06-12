1440d4515735f189b7a73c307fdf7990234aeec6
galaxyChen <549278303@qq.com>
2018-06-12 16:08:14 +0800

    添加登录后的导航栏

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <NavTop></NavTop>
+  <NavTop :logined='logined' :user='user'></NavTop>
 </template>
 
 
 <script>
 import NavTop from '~/components/NavTop'
 export default {  
+    data(){
+      return {
+        logined:true,
+        user:{
+          name:'张三'
+        }
+      }
+    },
     components:{
       NavTop
     },
