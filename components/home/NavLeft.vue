53753031f709d0eed97c1027ad7ab9a29b54fefc
galaxyChen <549278303@qq.com>
2018-06-12 17:24:08 +0800

    添加个人中心页面，添加个人信息管理界面

---
+++ components/home/NavLeft.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-menu
+        default-active="1"
+        @open="handleOpen"
+        @close="handleClose">
+
+        <el-menu-item index="1">
+            <i class="el-icon-star-on"></i>
+            <span slot='title'>个人信息管理</span>
+        </el-menu-item>
+
+        <el-menu-item index="2" >
+            <i class="el-icon-document"></i>
+            <span slot="title">地址簿管理</span>
+        </el-menu-item>
+
+        <el-submenu index="3">
+            <template slot="title">
+                <i class="el-icon-star-on"></i>
+                <span>交易管理</span>
+            </template>
+            <el-menu-item-group>
+            <el-menu-item index="3-1">购买管理</el-menu-item>
+            <el-menu-item index="3-2">出售管理</el-menu-item>
+            </el-menu-item-group>
+        </el-submenu>
+
+
+        <el-menu-item index="4">
+            <i class="el-icon-message"></i>
+            <span slot="title">消息管理</span>
+        </el-menu-item>
+    </el-menu>
+</template>
+
+<script>
+export default {
+    data(){
+        return {}
+    }
+}
+</script>
---
+++ pages/home/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <NavBar :logined='logined' :user='user'></NavBar>
+        <el-col :span="5">
+            <NavLeft></NavLeft>
+        </el-col>
+        <el-col :span='10' :push='3'>
+            <Person :user='user'></Person>
+        </el-col>
+    </div>
+</template>
+
+<script>
+import NavBar from "~/components/NavTop";
+import NavLeft from "~/components/home/NavLeft"
+import Person from "~/components/home/Person"
+export default {
+  components: {
+    NavBar,NavLeft,Person
+  },
+  data() {
+    return {
+        logined:true,
+        user:{
+            name:'张三'
+        }
+    };
+  }
+};
+</script>
