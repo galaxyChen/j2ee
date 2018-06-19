200badf15e380a2e1589678887d736380f074919
l <l>
2018-06-19 08:59:15 +0800

    619 859

---
+++ components/NavTop.vue
@@ -0,0 +1,166 @@
+<template>
+    <div>
+      <div v-if="login">
+        <el-menu
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color='None'
+          >
+          
+          <el-menu-item index="logo" class="NavLeft">二手书交易平台</el-menu-item>
+          <el-menu-item @click="signout" index="signout" class="NavRight">退出登录</el-menu-item>
+          <el-menu-item index="home" class="NavRight">个人中心</el-menu-item>
+          <el-menu-item index="index" class="NavRight">首页</el-menu-item>
+          <el-menu-item index="welcome" class="NavRight" >欢迎，{{user.name}}</el-menu-item>
+        
+        </el-menu>
+        <Login ref='login' :dialogVisible='dialogVisible'></Login>
+      </div>
+    <div v-else>
+      <el-menu
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color='None'
+        >
+        <el-menu-item index="home" class="NavLeft">二手书交易平台</el-menu-item>
+        <el-menu-item index="login" class="NavRight">登录/注册</el-menu-item>
+        <el-menu-item index="index" class="NavRight">首页</el-menu-item>
+      
+      </el-menu>
+      <Login ref='login' @logined="logined"  :dialogVisible='dialogVisible'></Login>
+    </div>
+    </div>
+</template>
+
+<style>
+.NavRight {
+  float: right !important;
+}
+.NavLeft {
+  font-size: 20px;
+}
+</style>
+
+
+<script>
+import Login from "~/components/loginRegister";
+import Cookies from "js-cookie";
+export default {
+  mounted() {
+    //事件注册
+    this.$on('changeName',this.changeName)
+    //预登录
+    let user_id = Cookies.get("user_id");
+    if (user_id) {
+      let data = {
+        query: "check",
+        data: {
+          user_id: user_id,
+          session_id: Cookies.get("session_id")
+        }
+      };
+      let check = this.$axios.send(data);
+      check.then(response => {
+        if (response.status === 1) {
+          this.login = true;
+          this.user.name = Cookies.get("name");
+          this.user.user_id = Cookies.get("user_id");
+        }
+      });
+    }
+  },
+  components: {
+    Login
+  },
+  data() {
+    return {
+      login: false,
+      user: {
+        name: "张三",
+        user_id: "1"
+      },
+      dialogVisible: false
+    };
+  },
+  methods: {
+    changeName(){
+      let name = Cookies.get('name')
+      this.user.name = name;
+    },
+    async handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+      if (key === "login") {
+        this.$refs.login.$emit("openDialog");
+      }
+
+      if (key === "home") {
+        let user_id = Cookies.get("user_id");
+        let session_id = Cookies.get("session_id");
+        if (user_id && session_id) {
+          let data = {
+            query: "check",
+            data: {
+              user_id: user_id,
+              session_id: session_id
+            }
+          };
+          let check = await this.$axios.send(data);
+          if (check.status === 1) {
+            this.$router.push({ path: `/home/` });
+          } else {
+            this.$message.error("操作失败!请重新登录");
+            Cookies.remove("name");
+            Cookies.remove("user_id");
+            Cookies.remove("session_id");
+            this.login = false;
+            this.user = {};
+            this.$router.push({ path: "/" });
+          }
+        }
+      }
+
+      if (key === "index") {
+        this.$router.push({ path: "/" });
+      }
+    },
+<<<<<<< HEAD
+    logined(){
+      let name = Cookies.get('name')
+      if (name){
+=======
+    logined() {
+      console.log("haha");
+      let name = Cookies.get("name");
+      if (name) {
+>>>>>>> e0c305d98e6712e2b96ab973aef38d86120f595b
+        this.user.name = name;
+        this.login = true;
+      }
+    },
+    async signout() {
+      console.log("sign out");
+      let user_id = Cookies.get("user_id");
+      let data = {
+        query: "signout",
+        data: {
+          user_id: user_id
+        }
+      };
+      let response = await this.$axios.send(data);
+      if (response.status === 0) {
+        this.$message.error(""+response.err);
+      }
+      Cookies.remove("name");
+      Cookies.remove("user_id");
+      Cookies.remove("session_id");
+      this.login = false;
+      this.user = {};
+      this.$router.push({ path: "/" });
+    }
+  }
+};
+</script>
---
+++ components/loginRegister.vue
@@ -0,0 +1,282 @@
+<template>
+
+    <el-dialog title="用户登录注册" :visible.sync="visible" @closed='closeDialog' width="30%" > 
+
+        <el-tabs v-model="query" type="card" >
+            <el-tab-pane label="登录" name="login"></el-tab-pane>
+            <el-tab-pane label="注册" name="register"></el-tab-pane>
+
+            <el-form  ref="loginForm" status-icon :model="loginForm" :rules="loginRules"  label-width="80px" v-show="query=='login'">
+                <el-form-item label="登录邮箱" prop="usn">
+                    <el-input  @input='check("loginForm","usn")' v-model="loginForm.usn"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="pw">
+                    <el-input  @input='check("loginForm","pw")' type="password" v-model="loginForm.pw"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('loginForm')">确定</el-button>
+                    <el-button @click="resetForm('loginForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+
+            <el-form  ref="regForm" status-icon :model="regForm"  :rules="regRules"  label-width="80px" v-show="query=='register'">
+                <el-form-item label="注册邮箱" prop="email">
+                    <el-input  @input='check("regForm","email")'  v-model="regForm.email"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="pw">
+                    <el-input  @input='check("regForm","pw")' type="password" v-model="regForm.pw"></el-input>
+                </el-form-item>
+
+                <el-form-item label="确认密码" prop="pw2"> 
+                    <el-input @input='check("regForm","pw2")' type="password" v-model="regForm.pw2"></el-input>
+                </el-form-item>
+                
+                <el-form-item label="昵称" prop="usn">
+                    <el-input @input='check("regForm","usn")' v-model="regForm.usn"></el-input>
+                </el-form-item>
+                <el-form-item label="密保问题" prop="question">
+                    <el-input @input='check("regForm","question")' type="textarea" maxlength=100 v-model="regForm.question" placeholder="不超过100个字符"></el-input>
+                </el-form-item>
+
+                <el-form-item label="密保回答" prop="answer">
+                    <el-input @input='check("regForm","answer")' type="textarea" maxlength=50 v-model="regForm.answer" placeholder="不超过50个字符"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('regForm')">确定</el-button>
+                    <el-button @click="resetForm('regForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+            
+        </el-tabs>
+
+    </el-dialog>
+</template>
+
+
+<style>
+.el-dropdown {
+  vertical-align: top;
+}
+.el-dropdown + .el-dropdown {
+  margin-left: 15px;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+</style>
+
+
+<script>
+import Cookies from 'js-cookie'
+
+export default {
+  mounted() {
+    this.$on("openDialog", function() {
+      this.visible = true;
+    });
+    this.$on("closeDialog", function() {
+      this.visible = false;
+    });
+    
+  },
+  data() {
+    var validatePw = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      }
+      else if(value.length>16 || value.length <5){
+        callback(new Error("长度在 5 到 16 个字符"));
+      } 
+      else {
+        let p = /[0-9]/ ;
+        let p1 = /[a-zA-Z]/i
+        if(!p.test(value)){
+          callback(new Error("密码至少包含数字和字母"));
+        }
+        else if (this.regForm.pw2 !== "") {
+          this.$refs.regForm.validateField("pw2");
+        }
+        callback();
+      }
+    };
+    var validatePw2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.regForm.pw) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      visible: false,
+      loginForm: {
+        usn: "",
+        pw: ""
+      },
+      regForm: {
+        usn: "",
+        email: "",
+        pw: "",
+        pw2: "",
+        question: "",
+        answer: ""
+      },
+      query: "login",
+      loginRules: {
+        usn: [
+          {
+            type: "email",
+            required: true,
+            message: "请输出正确的邮箱",
+            trigger: "blur"
+          },
+          { min: 1, max: 30, message: "长度小于30个字符", trigger: "blur" }
+        ],
+        pw: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 5, max: 16, message: "长度在 5 到 16 个字符", trigger: "blur" }
+        ]
+      },
+      regRules: {
+        email: [
+          {
+            type: "email",
+            required: true,
+            message: "请输出正确的邮箱",
+            trigger: "blur"
+          },
+          { min: 1, max: 30, message: "长度小于30个字符", trigger: "blur" }
+        ],
+        usn: [
+          { required: true, message: "昵称不能为空", trigger: "blur" },
+          { min: 1, max: 20, message: "长度小于20个字符", trigger: "blur" }
+        ],
+        pw: [{ required: true, validator: validatePw, trigger: "blur" }],
+        pw2: [{ required: true, validator: validatePw2, trigger: "blur" }],
+        question: [
+          { required: true, message: "密保问题不能为空", trigger: "blur" },
+          { min: 1, max: 100, message: "长度小于100个字符", trigger: "blur" }
+        ],
+        answer: [
+          { required: true, message: "密保回答不能为空", trigger: "blur" },
+          { min: 1, max: 50, message: "长度小于50个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    check(formName,item) {
+      console.log(this.$refs[formName])
+      this.$refs[formName].validateField(item)
+    },
+    submitForm: function(formName) {
+      console.log("login"+formName)
+      function serialize(obj) {
+        let result = {};
+        for (let term in obj) {
+          if (obj.hasOwnProperty(term)) {
+            result[term] = obj[term];
+          }
+        }
+        return result;
+      }
+      
+      this.$refs[formName].validate(async valid => {
+        
+        if (valid) {
+          let data = {
+            query: this.query
+          };
+          
+          if (data.query == "login") data.data = serialize(this.loginForm);
+          else if (data.query == "register")
+            data.data = serialize(this.regForm);
+<<<<<<< HEAD
+
+          console.log("test")
+
+          if(data.query=="login")
+            this.applyLogin(data)
+
+          else if(data.query == "register")
+            this.applyLogin(data)
+=======
+          let response = await this.$axios.send(data);
+
+          if(data.query=="login")
+            this.applyLogin(response)
+          else if(data.query == "register"){
+            if(this.applyRegister(response)){
+              let newData = {
+                query :"login",
+                usn : data.usn,
+                pw : data.pw
+              }
+              let response = await this.$axios.send(newData);
+              this.applyLogin(response)
+            }
+          }
+>>>>>>> e0c305d98e6712e2b96ab973aef38d86120f595b
+        
+        } else {
+          console.log("error");
+        }
+      });
+    },
+
+
+    async applyLogin(data){
+
+      let response = await this.$send(data);
+
+      if (response.status===1){
+        console.log("login success")
+        Cookies.set('user_id',response.data.user_id);
+        Cookies.set('name',response.data.name);
+        Cookies.set('session_id',response.data.session_id);
+        this.$emit("logined")
+
+        this.visible = false;
+      } else {
+        console.log("error")
+<<<<<<< HEAD
+        this.$message.error('错了哦，这是一条登录错误消息');
+      } 
+ 
+=======
+        this.$message.error('发生错误：'+response.err);
+      }  
+>>>>>>> e0c305d98e6712e2b96ab973aef38d86120f595b
+    },
+    async applyRegister(data){
+      
+      if (response.status===1){
+        console.log("register success")
+        let newData = {
+          query :"login",
+          usn : data.usn,
+          pw :data.pw
+        }
+        this.applyLogin(newData)
+      } else {
+        console.log("error")
+<<<<<<< HEAD
+        this.$message.error('错了哦，这是一条注册错误消息');
+=======
+        this.$message.error('发生错误：'+response.err);
+        return false;
+>>>>>>> e0c305d98e6712e2b96ab973aef38d86120f595b
+      }  
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    closeDialog() {
+      this.$refs["regForm"].resetFields();
+      this.$refs["loginForm"].resetFields();
+    }
+  }
+};
+</script>
