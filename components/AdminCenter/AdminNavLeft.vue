67740d4a490d5b2a9b90de65bd5cc4a473e34d44
softhuafei <114866525@qq.com>
2018-07-02 19:57:44 +0800

    增加客服登录、查看申诉列表用例

---
+++ components/AdminCenter/AdminNavLeft.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-menu @select='changeTab' :default-active="active">
+        <el-menu-item index="1">
+            <i class="el-icon-star-on"></i>
+            <span slot='title'>处理申诉</span>
+        </el-menu-item>
+
+    </el-menu>
+</template>
+
+<script>
+export default {
+    mounted(){
+        console.log(this.active)
+    },
+    props:['active'],
+    data(){
+        return {}
+    },
+    methods:{
+        changeTab(index,indexPath){
+            // console.log(index,indexPath)
+            this.$emit('changeTab',index,indexPath)
+        }
+    }
+
+}
+</script>
---
+++ pages/AdminCenter/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <AdminNavTop></AdminNavTop>
+        <el-col :span="5">
+            <AdminNavLeft :active='currentMainIndex' @changeTab='changeTab'></AdminNavLeft>
+        </el-col>
+        <component :is="currentMain"></component>
+    </div>    
+
+</template>
+<script>
+import AdminNavTop from "~/components/AdminCenter/AdminNavTop"
+import AdminNavLeft from "~/components/AdminCenter/AdminNavLeft"
+import DealComplain from "~/components/AdminCenter/DealComplain"
+export default {
+    components: {
+        AdminNavTop,
+        AdminNavLeft,
+        DealComplain
+    },
+    async mounted(){
+
+    },
+    data(){
+        return {
+            currentMain:'DealComplain',
+            currentMainIndex: '1',
+        }
+    },
+    methods: {
+        changeTab(index, indexPath) {
+        // console.log(index, indexPath);
+        let name = {
+            '1':"DealComplain",
+        };
+        this.currentMain = name[index];
+        this.currentMainIndex = index;
+        let adminId = Cookies.get('AdminId')
+        // this.$router.push({ path: `/home/${userId}` ,query:{index:index}});
+    },
+  }  
+};
+</script>
