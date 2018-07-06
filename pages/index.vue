fde10a69db840bf769634272bd7ae5cf951f182a
galaxyChen <549278303@qq.com>
2018-07-06 15:19:49 +0800

    修改首页显示的bug

--- pages/index.vue
+++ pages/index.vue
@@ -22,10 +22,10 @@
               <el-col :span="3" v-for="book in newBooks" :key='"recent"+book.itemId' :offset="1">
                 <el-card :body-style="{ padding: '10px' }" class="goods" >
                   <img @click='lookDetail(book.itemId)'  class="image" :src="book.pictureAddress">
-                  <div style="text-align:left;padding-left:10px;margin-top:-50px">
-                    <span style="font-weight:900;font-size:18px;"> {{book.itemTitle}}</span>
-                    <div class="bottom clearfix" @click='lookDetail(book.itemId)'>
-                      <p style="margin-top:-60px;font-weight:500;color:red;">￥ {{book.price}}</p>
+                  <div style="text-align:left;padding-left:10px;margin-top:20px;">
+                    <div class="recent-title"> {{book.itemTitle}}</div>
+                    <div class="bottom" @click='lookDetail(book.itemId)'>
+                      <p style="font-weight:500;color:red;">￥ {{book.price}}</p>
                       <el-button type="text" @click='lookDetail(book.itemId)' class="button">查看详情</el-button>
                     </div>
                   </div>
@@ -78,7 +78,10 @@ export default {
       this.$router.push({ path: `/item/${item_id}` });
     },
     doSearch(text, tag) {
-      this.$router.push({ path: "/search", query: { text: text, tag: tag ,time_stamp:new Date()-0} });
+      this.$router.push({
+        path: "/search",
+        query: { text: text, tag: tag, time_stamp: new Date() - 0 }
+      });
     }
   },
   watch: {
@@ -90,17 +93,25 @@ export default {
 </script>
 
 <style scoped>
-
 /* body {
   background-color: #050505;
   font-family: Hiragino Sans GB;
 } */
 
+.recent-title {
+  font-weight: 900;
+  font-size: 18px;
+  line-height: 20px;
+  height: 20px;
+  overflow: hidden;
+}
+
 .index-input-box {
   font-size: 20px;
 }
 
-.el-header,.el-footer {
+.el-header,
+.el-footer {
   background-color: #b3c0d1;
   color: #333333;
   /* text-align: center; */
@@ -114,32 +125,26 @@ export default {
   line-height: 160px;
 }
 
-body > .el-container {
-  margin-bottom: 40px;
-}
 .el-input__inner {
   height: 70px;
 }
 
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 400px;
-    
-  }
-  .el-carousel__item:nth-child(2n) {
-    background-color:white;
-    background-image: url("../assets/6.jpg");
-    height: 430px;
-
-  }
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-    background-image: url("../assets/7.jpg");
-    height: 430px; 
-     
-  }
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  line-height: 400px;
+}
+.el-carousel__item:nth-child(2n) {
+  background-color: white;
+  background-image: url("../assets/6.jpg");
+  height: 430px;
+}
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+  background-image: url("../assets/7.jpg");
+  height: 430px;
+}
 .goods {
   float: left;
   margin: 15px 1%;
@@ -150,43 +155,31 @@ body > .el-container {
 
 .search-box {
   margin-top: -50px;
-  padding:10px;
+  padding: 10px;
 }
-.service-box{
-  margin:0 20px 30px 20px;
+.service-box {
+  margin: 0 20px 30px 20px;
   background-color: white;
-  
 }
-.show-box{
-  margin:0 20px 0 20px;
+.show-box {
+  margin: 0 20px 0 20px;
   background-color: rgb(240, 239, 239);
 }
 
-
-
-
 .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-    padding-bottom: 0px;
-    padding-right: 15px;
-  }
-  .button {
-    margin-top: -30px;
-    padding: 0;
-    float: right;
-  }
-  .image {
-    width: 210px;
-    height: 280px;
-    display: block;
-  }
-  .clearfix:before,
-  .clearfix:after {
-      display: table;
-      content: "";
-  }
-  .clearfix:after {
-      clear: both
-  }
+  margin-top: 13px;
+  line-height: 12px;
+  padding-bottom: 0px;
+  /* padding-right: 15px; */
+}
+.button {
+  margin-top: -10px;
+  padding: 0;
+  float: right;
+}
+.image {
+  width: 210px;
+  height: 280px;
+  display: block;
+}
 </style>
